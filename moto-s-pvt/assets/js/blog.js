0704cdb628ed5d1c22dc62a252f88bcbf213338f
Harshit kumar <106154167+harshitkumar2003@users.noreply.github.com>
2024-04-17 00:58:25 +0530

    fix debugs in blog

--- moto-s-pvt/assets/js/blog.js
+++ moto-s-pvt/assets/js/blog.js
@@ -1,13 +1,13 @@
-// function readMore(postId) {
-//     const blogPost = document.getElementById(postId);
-//     const blogContent = blogPost.querySelector('.blog-content');
-//     const readMoreBtn = blogPost.querySelector('.read-more-btn');
-  
-//     if (blogContent.style.maxHeight) {
-//       blogContent.style.maxHeight = null;
-//       readMoreBtn.textContent = 'Read More';
-//     } else {
-//       blogContent.style.maxHeight = blogContent.scrollHeight + 'px';
-//       readMoreBtn.textContent = 'Read Less';
-//     }
-//   }
+function readMore(postId) {
+    const blogPost = document.getElementById(postId);
+    const blogContent = blogPost.querySelector('.blog-content');
+    const readMoreBtn = blogPost.querySelector('.read-more-btn');
+
+    if (blogContent.style.maxHeight) {
+        blogContent.style.maxHeight = null;
+        readMoreBtn.textContent = 'Read More';
+    } else {
+        blogContent.style.maxHeight = blogContent.scrollHeight + 'px';
+        readMoreBtn.textContent = 'Read Less';
+    }
+}
